48606cd90cb8e99a94e616f52c65015b14b21165
hi <kangja1996@gmail.com>
2020-10-28 21:26:22 -0400

    progress

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hey Clever Programmer Fam, let's bulid the Google Clone!</h1>
+
+      {/* Home (the one with the search one) */}
+
+
+      {/* SearchPage (The results page) */}
+      
+
     </div>
   );
 }

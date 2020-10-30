90ceffe26a7773a6b6e75e69d2a41d67a3f41bea
hi <kangja1996@gmail.com>
2020-10-29 22:14:49 -0400

    progress

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css"
-import Home from "./pages/Home"
+import Home from "./Home"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,19 +9,12 @@ function App() {
       <Router>
         <Switch>
           <Route path="/search">
-            <SearchPage/>
+            <h1>This is the search page</h1> 
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-        <h1>Hey Clever Programmer Fam, let's bulid the Google Clone!</h1>
-
-        {/* Home (the one with the search one) */}
-        <Home />
-
-        {/* SearchPage (The results page) */}
-
       </Router>
 
        
--- google-clone/src/Home.js
+++ google-clone/src/Home.js
@@ -1,24 +1,22 @@
 // rfce
 import React from 'react'
 import "./Home.css"
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="home">
-      <h1>This is HOMEpage</h1>
-
+    <div className="home"> 
       <div className="home__header">
         <div className="home__headerLeft">
-          {/* Link */}
-          {/* Link */}
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
         <div className="home__headerRight">
-          {/* Link */}
-          {/* Link */}
+        <Link to="/gmail">Gmail</Link>
+        <Link to="/images">Images</Link>
           {/* Icon */}
           {/* Avatar */}
         </div>
-
       </div>
 
       <div className="home__body">

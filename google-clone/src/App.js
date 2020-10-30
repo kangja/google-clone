f7d848dea287636a1b06289f697c6d713d50faf6
hi <kangja1996@gmail.com>
2020-10-29 21:04:09 -0400

    process

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import './App.css';
+import "./App.css"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         <h1>Hey Clever Programmer Fam, let's bulid the Google Clone!</h1>
 
-      {/* Home (the one with the search one) */}
+        {/* Home (the one with the search one) */}
+        <Home />
 
+        {/* SearchPage (The results page) */}
 
-      {/* SearchPage (The results page) */}
-      
+      </Router>
+
+       
 
     </div>
   );
---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,31 @@
+// rfce
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is HOMEpage</h1>
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+
+      </div>
+
+      <div className="home__body">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home

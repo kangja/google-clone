8226022e7fbfbcfcce850a5b60612fb2f481a043
hi <kangja1996@gmail.com>
2020-11-19 00:19:53 -0500

    progress

--- google-clone/src/Search.js
+++ google-clone/src/Search.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Search.css"
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core"; 
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+
+    console.log("you hit the search button >> ", input)
+  };
+   
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={e => setInput(e.target.value)}/>
         <MicIcon />
       </div>
 
       <div className="search__buttons">
-        <Button>Google Search</Button>
-        <Button variant="outlined">I 'm Feeling Lucky</Button>
-
+        <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   )
 }
 

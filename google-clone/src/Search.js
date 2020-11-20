931e2af6bb519ded9efb275a10131be167086859
hi <kangja1996@gmail.com>
2020-11-20 18:25:34 -0500

    progress

--- google-clone/src/Search.js
+++ google-clone/src/Search.js
@@ -3,14 +3,18 @@ import "./Search.css"
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core"; 
+import { useHistory } from "react-router-dom";
 
-function Search() {
+function Search( { hideButtons = false }) {
   const [input, setInput] = useState("");
+  const history = useHistory();
 
   const search = (e) => {
     e.preventDefault();
 
     console.log("you hit the search button >> ", input)
+
+    history.push("/search");
   };
    
   return (
@@ -21,10 +25,18 @@ function Search() {
         <MicIcon />
       </div>
 
-      <div className="search__buttons">
+      {!hideButtons ? (
+        <div className="search__buttons">
         <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
+      ) : (
+        <div className="search__buttons">
+        <Button className="search__buttonsHidden" type="submit" onClick={search} variant="outlined">Google Search</Button>
+        <Button className="search__buttonsHidden" variant="outlined">I'm Feeling Lucky</Button>
+      </div>
+      )}
+      
     </form>
   )
 }
